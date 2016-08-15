b0be1f4f052ea4fe536516f5cae5bb0ecb816809
Randi Mays <randimays1024@gmail.com>
2016-08-15 13:10:55 -0500

    Final styling to navbar

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,19 +1,7 @@
 <header>
-	<div id="links-bar">
-		<div id="logo-div-top">
-			<a href="/"><img id="logo-top" src="/img/Fantasy-Costco-by-Ryanphantom.png"></a>
-		</div>
-	</div>
 	<div id="main-header-bar">
 		<div id="logo-div">
 			<a href="/"><img id="logo" src="/img/Fantasy-Costco-by-Ryanphantom.png"></a>
-		</div>
-		<div id="small-search">
-			<form method="GET" style="margin-top:0px">
-			<div style="display:block;position:relative;width:100%;background-color:purple;height:30px;min-width:65px;min-width:70px;max-width:70px;padding:5px;border:1px solid black">Shop
-				<input type="image" id="list-icon" src="/img/interface.png" style="background-color:yellow">
-			</form>
-			</div>
 		</div>
 		<div id="search-bar-div">
 			<div id="search-bar-inner-div">
@@ -23,35 +11,33 @@
 			</form>
 			</div>
 		</div>
-		<div id="large-content-menu">
-				<div id="sign-in-div">
-				<span id="sign-in-span">Sign In/Register</span>
-				</div>
-				<div id="cart-div">
-				<span id="cart-span">Cart</span>
-				<img id="cart-logo" src="/img/commerce.svg">
-				</div>
-		</div>
 		<div id="medium-content-menu">
-		<div class="medium-content-item" style="">
-		<img src="/img/location-pin.svg" style="position:relative;width:22px;left:40px">
-		<span style="position:relative;top:10px">Locations</span>
-		</div>
-		<div class="medium-content-item" style="border-right:1px solid #3071a9">
-			<img src="/img/user-silhouette.svg" style="position:relative;width:22px;left:40px">
-			<span style="position:relative;top:10px">Account</span>
-		</div>
-		<div class="medium-content-item" style="">
-			<img src="/img/commerce.svg" style="position:relative;width:22px;left:40px">
-			<span style="position:relative;top:10px;left:12px">Cart</span>
-		</div>
+			<div class="iconDiv pin">
+				<img class="icon" src="/img/location-pin.svg">
+				<span class="iconText">Locations</span>
+			</div>
+			<div class="iconDiv user">
+				<img class="icon" src="/img/user-silhouette.svg">
+				<span class="iconText">Account</span>
+			</div>
+			<div class="iconDiv cart">
+				<img class="icon" src="/img/commerce.svg">
+				<span class="iconText">Cart</span>
+			</div>
 		</div>
 	</div>
 	<div id="search-div">
-		<img id="list-icon" src="/img/interface.png">
-		<span><a href="/?category=Consumable" class="option">Consumables</a></span>
-		<span><a href="/?category=Equipment"class="option">Equipment</a></span>
-		<span><a href="/?category=Misc" class="option">Misc</a></span>
-		<span><a href="/?category=Weapon" class="option">Weapons</a></span>
+		<div class="leftMenu">
+			<a href="/?category=Consumable" class="option">Consumables</a>
+			<a href="/?category=Equipment" class="option">Equipment</a>
+			<a href="/?category=Weapon" class="option">Weapons</a>
+			<a href="/?category=Misc" class="option">Misc</a>
+		</div>
+		<div class="rightMenu">
+			<?php if (isset($_SESSION["LOGGED_IN_ID"])) : ?>
+				<a href="logout.php" class="option">Log Out</a>
+			<?php endif; ?>
+		</div>
+
 	</div>
 </header>
